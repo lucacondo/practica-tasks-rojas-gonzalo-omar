cde0cb5f94843fbeff4d6588a8857e805ebce0e8
lucacondo <rojasgonzaloomar@gmail.com>
2026-08-17 21:44:50 -0300

    fix: se trabajó con las rutas y controladores en la rama equivocada

--- src/routes/user.route.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-export const userRouter = Router();
-
-userRouter.get("/users", getAllUsers);
-userRouter.get("/users/:id", getUserById);
-userRouter.post("/users", createUser);
-userRouter.put("/users/:id", updateUser);
-userRouter.delete("/users/:id", deleteUser);
