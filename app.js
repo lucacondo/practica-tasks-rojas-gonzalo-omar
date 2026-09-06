05fc32867246f745d99f3c70364a668634d29dae
lucacondo <rojasgonzaloomar@gmail.com>
2026-09-06 13:55:59 -0300

    feat: Se levanto el servidor

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import { startDB } from "./src/config/database.js";
+import { userRouter } from "./src/routes/user.route.js";
+import { taskRouter } from "./src/routes/task.route.js";
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use("/api", taskRouter);
+app.use("/api", userRouter);
+
+app.listen(PORT, async () => {
+    console.log(`Servidor conectado en el puerto ${PORT}`);
+    await startDB();
+    console.log("Conectado a la base de datos");
+})
